e37830ab94b91c9769d3ba9dc0464729ac291da3
sumin <sumin1667@gmail.com>
2024-03-22 17:35:30 +0900

    layout design without function

--- lib/screens/consult_screen.dart
+++ lib/screens/consult_screen.dart
@@ -12,15 +12,104 @@ class _ConsultScreenState extends State<ConsultScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(),
-      body: Center(
-        child: Container(
-          color: Colors.lightBlue,
-          child: const Center(
-            child: Text(
-              '상담페이지',
-              style: TextStyle(fontSize: 24, color: Colors.white),
+      body: Padding(
+        padding: const EdgeInsets.all(24.0),
+        child: Column(
+          // todo : flexible 사용해서 비율로 위젯 비율 맞추기
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: <Widget>[
+            Icon(Icons.multitrack_audio_outlined, color: const Color(0xff0045FF).withOpacity(0.6),),
+            SizedBox(height: 20,),
+            Text('추천받은 상품이 있으시군요?', style: TextStyle(color: const Color(0xff0045FF), fontSize: 26),),
+            Text('상품 이름을 알려주세요!', style: TextStyle(color: const Color(0xff0045FF), fontSize: 26),),
+            SizedBox(height: 50,),
+            // todo : 검색 기능 연결하기
+            Container(
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(10.0),
+                color: Colors.white,
+              ),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Text('상품 이름을 입력해주세요', style: TextStyle(color: const Color(0xff9BA6B4)),),
+                  Icon(Icons.search, color: const Color(0xff0045FF)),
+                ],
+              ),
             ),
-          ),
+            SizedBox(height: 40,),
+            Container(
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(10.0),
+                color: Colors.white,
+              ),
+              child: Column(
+                children: [
+                  Text('아는만큼 보인다!'),
+                  Text('당신을 위한 투자 길라잡이'),
+                  SizedBox(height: 18,),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Row(
+                        children: [
+                          Container(
+                            height: 24,
+                            width: 24,
+                            color: Color(0xff3168FD),
+                            child: Text('1'),
+                          ),
+                          SizedBox(width: 10,),
+                          Text('ELS(주가지수 연계형 상품)'),
+                        ],
+                      ),
+                      Icon(Icons.arrow_forward_ios_sharp)
+                    ],
+                  ),
+                  SizedBox(height: 5,),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Row(
+                        children: [
+                          Container(
+                            height: 24,
+                            width: 24,
+                            color: Color(0xff3168FD),
+                            child: Text('1'),
+                          ),
+                          SizedBox(width: 10,),
+                          Text('ELS(주가지수 연계형 상품)'),
+                        ],
+                      ),
+                      Icon(Icons.arrow_forward_ios_sharp)
+                    ],
+                  ),
+                  SizedBox(height: 5,),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Row(
+                        children: [
+                          Container(
+                            height: 24,
+                            width: 24,
+                            color: Color(0xff3168FD),
+                            child: Text('1'),
+                          ),
+                          SizedBox(width: 10,),
+                          Text('ELS(주가지수 연계형 상품)'),
+                        ],
+                      ),
+                      Icon(Icons.arrow_forward_ios_sharp)
+                    ],
+                  ),
+                  SizedBox(height: 20,),
+                ],
+              ),
+            )
+          ]
         ),
       ),
     );
