d37d8f4356874101368a9a935ebe24df7989d802
yerim <97941141+yerim123456@users.noreply.github.com>
2024-04-03 18:09:28 +0900

    fix : Modify background color

--- lib/screens/record/record_feature_wrong_popup.dart
+++ lib/screens/record/record_feature_wrong_popup.dart
@@ -7,92 +7,97 @@ class RecordWrongPopup extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return AlertDialog(
-      backgroundColor: Colors.white,
-      content: Container(
-        child: Column(
-          mainAxisSize: MainAxisSize.min,
-          children: [
-            SizedBox(height: 20),
-            Text(
-              "잘못된 설명입니다.",
-              style: TextStyle(
-                fontSize: 20,
-                fontWeight: FontWeight.w700,
-                color: Color(0xFF000000),
-              ),
-            ),
-            SizedBox(height: 16),
-            Text(
-              text,
-              textAlign: TextAlign.center,
-              style: TextStyle(
-                fontSize: 16,
-                fontWeight: FontWeight.w700,
-                color: Color(0xFFFE3A3B),
-              ),
-            ),
-            SizedBox(height: 16),
-            RichText(
-              text: TextSpan(
-                children: [
-                  TextSpan(
-                    text: "이 상품은 파생결합증권으로 예금과는 달리 예금자보호를 받지 않아요. 즉, 수익을 지급받지 못하거나",
-                    style: TextStyle(
-                      fontSize: 16,
-                      fontWeight: FontWeight.w400,
-                      color: Color(0xFF353535),
-                    ),
-                  ),
-                  TextSpan(
-                    text: " 원금의 최대 100% 까지 손실",
-                    style: TextStyle(
-                      fontSize: 16,
-                      fontWeight: FontWeight.w400,
-                      color: Colors.red,
-                    ),
-                  ),
-                  TextSpan(
-                    text: "이 발생할 수 있습니다.",
-                    style: TextStyle(
-                      fontSize: 16,
-                      fontWeight: FontWeight.w400,
-                      color: Color(0xFF353535),
-                    ),
-                  ),
-                ],
-              ),
-              textAlign: TextAlign.center,
-            ),
-            SizedBox(height: 28),
-            TextButton(
-              onPressed: () {
-                Navigator.of(context).pop();
-              },
-              child: Container(
-                width: double.infinity,
-                margin: EdgeInsets.symmetric(horizontal: 16),
-                child: Text(
-                  '확인',
-                  textAlign: TextAlign.center,
+    return Material(
+        color: Colors.black.withOpacity(0.5),
+        child: AlertDialog(
+          surfaceTintColor: Colors.white,
+          backgroundColor: Colors.white,
+          content: Container(
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                SizedBox(height: 20),
+                Text(
+                  "잘못된 설명입니다.",
                   style: TextStyle(
                     fontSize: 20,
                     fontWeight: FontWeight.w700,
-                    color: Color(0xFF878787),
+                    color: Color(0xFF000000),
                   ),
                 ),
-              ),
-              style: TextButton.styleFrom(
-                backgroundColor: Color(0xFFF2F4F6),
-                padding: EdgeInsets.symmetric(vertical: 16),
-                shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(10),
+                SizedBox(height: 16),
+                Text(
+                  text,
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                    fontSize: 16,
+                    fontWeight: FontWeight.w700,
+                    color: Color(0xFFFE3A3B),
+                  ),
                 ),
-              ),
+                SizedBox(height: 16),
+                RichText(
+                  text: TextSpan(
+                    children: [
+                      TextSpan(
+                        text:
+                            "이 상품은 파생결합증권으로 예금과는 달리 예금자보호를 받지 않아요. 즉, 수익을 지급받지 못하거나",
+                        style: TextStyle(
+                          fontSize: 16,
+                          fontWeight: FontWeight.w400,
+                          color: Color(0xFF353535),
+                        ),
+                      ),
+                      TextSpan(
+                        text: " 원금의 최대 100% 까지 손실",
+                        style: TextStyle(
+                          fontSize: 16,
+                          fontWeight: FontWeight.w400,
+                          color: Colors.red,
+                        ),
+                      ),
+                      TextSpan(
+                        text: "이 발생할 수 있습니다.",
+                        style: TextStyle(
+                          fontSize: 16,
+                          fontWeight: FontWeight.w400,
+                          color: Color(0xFF353535),
+                        ),
+                      ),
+                    ],
+                  ),
+                  textAlign: TextAlign.center,
+                ),
+                SizedBox(height: 28),
+                TextButton(
+                  onPressed: () {
+                    Navigator.of(context).pop();
+                  },
+                  child: Container(
+                    width: double.infinity,
+                    margin: EdgeInsets.symmetric(horizontal: 16),
+                    child: Text(
+                      '확인',
+                      textAlign: TextAlign.center,
+                      style: TextStyle(
+                        fontSize: 20,
+                        fontWeight: FontWeight.w700,
+                        color: Color(0xFF878787),
+                      ),
+                    ),
+                  ),
+                  style: TextButton.styleFrom(
+                    backgroundColor: Color(0xFFF2F4F6),
+                    padding: EdgeInsets.symmetric(vertical: 16),
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(10),
+                    ),
+                  ),
+                ),
+              ],
             ),
-          ],
-        ),
-      ),
+          ),
+        )
     );
   }
 }
